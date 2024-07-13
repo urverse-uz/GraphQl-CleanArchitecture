cfe6174ab01fe071cdba092dd909ea5f25a76408
Ismoiljon <hs.umarov21@gmail.com>
2024-07-13 20:44:26 +0500

    Initial commit

---
+++ app/src/main/java/uz/umarov/graphqlcountriesapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package uz.umarov.graphqlcountriesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import uz.umarov.graphqlcountriesapp.ui.theme.GraphQlCountriesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GraphQlCountriesAppTheme {
+
+            }
+        }
+    }
+}
